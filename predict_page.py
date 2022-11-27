5036f01de697617cb505609938c8e2032362611b
Soham Bhattacharjee <soham.onlyn@gmail.com>
2022-11-26 22:53:02 -0800

    adds dummy pages

---
+++ predict_page.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+
+def show_predict_page():
+    st.title("Predict stuff")
+
+    st.write(
+        """
+    ### Stack Overflow Developer Survey 2020
+    """
+    )
+
+
+    st.write("""#### Number of Data from different countries""")
+
+    st.write(
+        """
+    #### Mean Salary Based On Country
+    """
+    )
+
+    
+    st.write(
+        """
+    #### Mean Salary Based On Experience
+    """
+    )
+
+    
